d46049dc618f50d59b2b581b4278331d161b34d9
sitdintasca <35195792+sitdintasca@users.noreply.github.com>
2018-01-07 17:32:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ importScripts('/app/sw-toolbox.js');
 
 toolbox.precache(["/app/index.html","style/style.css"]);
 
-toolbox.router.get('/assets/*', toolbox.cacheFirst);
+toolbox.router.get('/app/assets/*', toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {
+toolbox.router.get('/app/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });

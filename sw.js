c728dbc2e37f072538cda74fd0dde4694b17ec01
sitdintasca <35195792+sitdintasca@users.noreply.github.com>
2018-01-07 17:42:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('/app/sw-toolbox.js');
 
-toolbox.precache(["/app/index.html","style/style.css"]);
+toolbox.precache(["/app/index2.html","style/style.css"]);
 
 toolbox.router.get('/app/assets/*', toolbox.cacheFirst);
 
